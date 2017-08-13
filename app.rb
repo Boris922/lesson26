f14a1b76da3a8a8a456bbc04498f38bb7efe9ddf
silent <silent161192@gmail.com>
2017-08-13 11:07:47 +0300

    t-t

--- app.rb
+++ app.rb
@@ -4,8 +4,9 @@ db = SQLite3::Database.new 'barbershop.db'
 
 db.execute 'select * from users' do |row|
 
-	puts row
-
+	puts "{row[1] записался на #{row[3]}"
+	print "\t-\t"
+	puts "#{row[2]}"
 	puts "======"
 
 end
